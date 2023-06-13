e14a658be8d734d3e9f4d7f71a9d6b26e2a5ecea
Muhammad424 <135837731+Muhammad424@users.noreply.github.com>
2023-06-13 22:48:52 +0700

    Update Coems.lua

--- Coems.lua
+++ Coems.lua
@@ -19,7 +19,7 @@ getgenv().Config = {
     },
     Misc = {
         ServerHop = true,
-        Disable3DRendering = false
+        Disable3DRendering = true
     }
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/c74cd38443b5d8e682d4bcd375609834.lua"))()
